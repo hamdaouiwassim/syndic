f9f9218a7a3da9f3a18984d45d4581cba7f217df
hamdaouiwassim <wassim.hamdaoui@wi-mobi.com>
2021-06-21 15:40:19 +0200

    adminSeed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\User;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,5 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        User::create([
+            'name' => 'admin',
+            'role' => 'ADMIN',
+            'password' => Hash::make('admin'),
+        ]);
     }
 }
